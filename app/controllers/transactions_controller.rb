3e3ce5cc8d9f440476c9c4f79a2ec2594a1d2e90
Ashok Kumar <k.ashok502@gmail.com>
2013-03-27 15:25:22 +0530

    implement the filter for transactions page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,23 @@
+require 'csv'
 class TransactionsController < ApplicationController
+
+  def index
+    @transactions = Transaction.where("user_id = #{current_user.id}")
+  end
+
+  def export_transactions
+    @transactions = Transaction.where("user_id = #{current_user.id}")
+    csv_string = CSV.generate do |csv|
+      csv << ["Address", "MLS Number", "Status","Close Date","More Info","Buyer","Seller","List price","Sale price","Commission Amount","Commission Summary"]
+      @transactions.each do |transaction|
+        csv << [transaction.transaction_name, transaction.transaction_number, transaction.status, transaction.close_date,transaction.more_info,transaction.buyer_name,transaction.seller_name,transaction.list_price,transaction.sale_price,transaction.total_commission,transaction.commission_summary]
+      end
+    end
+
+    send_data csv_string,
+      :type => 'text/csv; charset=iso-8859-1; header=present',
+      :disposition => "attachment; filename=trasactions.csv"
+  end
 
   def new
     @transaction = Transaction.new
@@ -7,10 +26,27 @@ class TransactionsController < ApplicationController
   def create
     @transaction = Transaction.new(params[:transaction])
     @transaction.user_id = current_user.id
+    @transaction.location_id = params[:transaction][:location_id]
     if @transaction.save
       redirect_to dashboard_index_path
     else
       render :action => 'new'
+    end
+  end
+
+  def search
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def location_search
+    if params[:location].present?
+      @transactions = Transaction.where("(location_id = #{params[:location]} or status = #{params[:status]}) and user_id = #{current_user.id}")
+      render :action => 'index'
+    else
+      @transactions = Transaction.where("user_id = #{current_user.id}")
+      render :action => 'index'
     end
   end
 end
