37f63855463a02476a4a3e59951cfe47cfb3e963
neeraji2it <Neerajonrails@gmail.com>
2013-04-10 17:27:28 +0530

    documents falsh msgs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class DashboardController < ApplicationController
   layout 'application'
   def index
-      @documents = Document.where("user_id = #{current_user.id}")
+      required_document_types = ["private","sale","listing"]
+      uploaded_document_types = current_user.documents.collect{|d| d.document_type.to_s.downcase}.uniq
+      missing_document_types = required_document_types - uploaded_document_types
+      if missing_document_types.present?
+        flash[:notice] = "Please Upload #{missing_document_types[0]} documents "
+      else
+        @documents = Document.where("user_id = #{current_user.id}")
+      end
   end
   
   def show
