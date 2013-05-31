acb456806e0559ed691c5d5104fdfa7ed28d610f
neeraji2it <Neerajonrails@gmail.com>
2013-05-31 15:22:01 +0530

    modified envt.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+if ENV['RAILS_ENV'] == "staging"
+  ActiveSupport::Deprecation.silenced = true
+end
+
 ActiveSupport::Deprecation.silenced = true 
 # Initialize the rails application
 Paperlesspipeline::Application.initialize!
