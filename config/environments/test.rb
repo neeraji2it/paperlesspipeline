3a7e69147c020b8a116cf5b81c1b573a3b7ccaf6
neeraji2it <Neerajonrails@gmail.com>
2013-07-16 14:34:46 +0530

    mohan

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,7 +14,7 @@ Paperlesspipeline::Application.configure do
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
 
-  config.assets.compile = fase
+  config.assets.compile = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
