401ceed819dd1e1876b140ffe8cc078c8542d9e4
neeraji2it <Neerajonrails@gmail.com>
2013-07-18 20:28:24 +0530

    fsd

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,39 +1,44 @@
 Paperlesspipeline::Application.configure do
-  config.cache_classes = false
+  # Settings specified here will take precedence over those in config/application.rb
 
-  # Log error messages when you accidentally call methods on nil.
+  # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+
+  # Configure static asset server for tests with Cache-Control for performance
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
+
+  # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
+
+  config.assets.compile = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # Raise exceptions instead of rendering exception templates
+  config.action_dispatch.show_exceptions = false
 
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
+  # Disable request forgery protection in test environment
+  config.action_controller.allow_forgery_protection    = false
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # Print deprecation notices to the stderr
+  config.active_support.deprecation = :stderr
 
-  # Do not compress assets
-  config.assets.compress = false
-  config.assets.compile = true
-
-
-  # Expands the lines which load the assets
   config.assets.debug = true
-  config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { :host => 'ec2-54-245-14-77.us-west-2.compute.amazonaws.com' }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
