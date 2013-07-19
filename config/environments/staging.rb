1fe6c043a6a5dc7a9df6d1c24682a8574dd2084a
neeraji2it <Neerajonrails@gmail.com>
2013-07-19 16:28:43 +0530

    fds

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-Project::Application.configure do
+Paperlesspipeline::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
@@ -30,6 +30,8 @@ Project::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = true
+
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
