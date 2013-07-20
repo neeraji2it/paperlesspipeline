80e4c5810093c514aba580805985907d9ccde685
neeraji2it <Neerajonrails@gmail.com>
2013-07-20 22:15:23 +0530

    mohan

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,6 +21,8 @@ Paperlesspipeline::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
+  config.action_dispatch.show_exceptions = true
+
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
