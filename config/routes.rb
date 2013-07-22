33b1c478e3bdbb5c392369f1c9eb619bdcae6a9f
neeraji2it <Neerajonrails@gmail.com>
2013-07-22 19:04:37 +0530

    mohan commit

--- config/routes.rb
+++ config/routes.rb
@@ -106,7 +106,11 @@ Paperlesspipeline::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+#  root :to => 'welcome#index'
+
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
 
   # See how all your routes lay out with "rake routes"
 
