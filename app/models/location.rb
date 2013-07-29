50b8cf7a9ef4249b3ce0000c16e08dfd10e4d8cc
neeraji2it <Neerajonrails@gmail.com>
2013-07-29 10:59:03 +0530

    mohan changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   attr_accessible :name, :user_id
-  belongs_to :user
+  has_many :users
   has_many :documents, :dependent => :destroy
   has_many :transactions, :dependent => :destroy
   has_many :users
