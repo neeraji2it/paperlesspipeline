c4ce34af63ffaad1659888241c2b48bd94868272
neeraji2it <Neerajonrails@gmail.com>
2013-08-28 15:21:54 +0530

    mohan

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,25 +3,21 @@ class DashboardController < ApplicationController
 
   layout 'application'
   def index
-    #render :layout => false
     required_document_types = ["private","sale","listing"]
     uploaded_document_types = current_user.documents.collect{|d| d.document_type.to_s.downcase}.uniq
     missing_document_types = required_document_types - uploaded_document_types
-    #    if missing_document_types.present?
-    #    else
     @documents = Document.where("user_id = '#{current_user.id}'")
 
-    # All Listing Transactions============================================================================
+    # Current month all transaction ======================================================================
     @transactions = Transaction.where("user_id = '#{current_user.id}'")
-    @total_commission = []
-    @transactions.each do |transaction|
-      @total_commission << transaction.total_commission
+    @this_month_listing_total_commission = []
+    @transactions.each do |i|
+      if i.present? and i.close_date.present?
+        if i.close_date.strftime("%m") == Date.today.strftime("%m") and i.close_date.strftime("%y") == Date.today.strftime("%y")
+          @this_month_listing_total_commission << i.total_commission
+        end
+      end
     end
-    @closed_total_commission = []
-    @transactions.each do  |i|
-      @closed_total_commission << i.total_commission
-    end
-    @closing_transactions = Transaction.where("user_id = '#{current_user.id}' and '#{Date.today}' < close_date < '#{Date.today+1.month}'")
 
     # Previous month transactions ========================================================================
     @passed_transactions = Transaction.where("user_id = '#{current_user.id}' and close_date < '#{Date.today}'")
@@ -39,19 +35,18 @@ class DashboardController < ApplicationController
       end
     end
 
-    # Current month all transaction ======================================================================
-    @this_month_listing_total_commission = []
-    @transactions.each do |i|
-      if i.present? and i.close_date.present?
-        if i.close_date.strftime("%m") == Date.today.strftime("%m") and i.close_date.strftime("%y") == Date.today.strftime("%y")
-          @this_month_listing_total_commission << i.total_commission
-        end
-      end
+    # All Listing Transactions============================================================================
+    @transactions = Transaction.where("user_id = '#{current_user.id}'")
+    @total_commission = []
+    @transactions.each do |transaction|
+      @total_commission << transaction.total_commission
     end
-    #    end
-  
-    def show
+    @closed_total_commission = []
+    @transactions.each do  |i|
+      @closed_total_commission << i.total_commission
     end
+    @closing_transactions = Transaction.where("user_id = '#{current_user.id}' and '#{Date.today}' < close_date < '#{Date.today+1.month}'")
 
   end
+
 end
