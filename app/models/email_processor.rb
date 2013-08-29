f9619e5845aad9e80d594e980a9cdf53514372dc
neeraji2it <Neerajonrails@gmail.com>
2013-08-29 13:15:17 +0530

    mohan

--- app/models/email_processor.rb
+++
@@ -1,8 +0,0 @@
-class EmailProcessor < ActiveRecord::Base
-  # attr_accessible :title, :body
-
-  def self.process(email)
-    Document.create(:document_file_name => email.body)
-  end
-
-end
--- test/functional/email_processor_test.rb
+++ test/functional/email_processor_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class EmailProcessorTest < ActiveSupport::TestCase
+class EmailProcessorTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
