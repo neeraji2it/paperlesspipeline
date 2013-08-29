d3598e333cb5d975dbe8ace8f1bf2ad2c2d0fc73
neeraji2it <Neerajonrails@gmail.com>
2013-08-29 11:32:40 +0530

    mohan

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -2,11 +2,7 @@ class EmailProcessor < ActiveRecord::Base
   # attr_accessible :title, :body
 
   def self.process(email)
-    d = Document.create(:document_file_name => email.body)
-    d.save
-    puts "==========================================================================="
-    puts d.inspect
-    puts "==========================================================================="
+    Document.create(:document_file_name => email.body)
   end
 
 end
